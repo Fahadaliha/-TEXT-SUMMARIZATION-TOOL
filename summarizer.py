ad1fb2e1c68c6d8e431b9d4d8407d1cff5aae0a6
Fahadaliha <xypsys123@gmail.com>
2025-10-27 17:22:12 +0530

    summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -11,6 +11,7 @@ def summarize(text,size):
     text=re.sub(r'\[[0-9*\]]',' ',text) #removes number references
 
     sentences = sent_tokenize(text)
+    totsentence = len(sentences) #to get len for summary length
     words = word_tokenize(text) # We tokenised sentece and words
 
     useless = set(stopwords.words('english'))# removes usless words like is or etc
@@ -20,18 +21,18 @@ def summarize(text,size):
         if word not in useless and word.isalpha():
             word_frequencies[word] = word_frequencies.get(word,0) + 1
     
-    sentence_score = {} #setence score for imp key words
+    sentence_score = {} #sentence score for imp key words
     for sent in sentences:
         for word in word_tokenize(sent.lower()):
             if word in word_frequencies:
                 sentence_score[sent]=sentence_score.get(sent,0) + word_frequencies[word]
 
     if size==1:
-        numb=max(8,len(sentences)//10)
+        numb=max(1,totsentence//7)
     elif size==2:
-        numb=max(16,len(sentences)//4)
+        numb=max(2,totsentence//3)
     else:
-        numb=max(12,len(sentences)//6) #here we set how much we want summary in size
+        numb=max(3,totsentence//8) #here we set how much we want summary in size
 
     summary_sentences=heapq.nlargest(numb,sentence_score,key=sentence_score.get)
     summary = ' '.join(summary_sentences)
